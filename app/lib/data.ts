f1c4a605f07bcea722dcce2148ddf2bd91fc8536
yuya <kinjo.bobchin@gmail.com>
2023-10-30 11:24:46 +0900

    chapter7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -90,7 +90,7 @@ export async function fetchCardData() {
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
   query: string,
-  currentPage: number,
+  currentPage: number
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import styles from './home.module.css';
 import Image from 'next/image';
 
 import Link from 'next/link';
@@ -7,9 +6,8 @@ import { lusitana } from './ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape}></div>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
