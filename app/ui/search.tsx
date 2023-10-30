ad97965c29374099ba7c7253f37e48afa6384fbf
yuya <kinjo.bobchin@gmail.com>
2023-10-30 19:04:48 +0900

    feat: Add create invoice page and form
    This commit adds a new file `page.tsx` under the `app/dashboard/invoices/create` directory. The file contains code to render the create invoice page, including a breadcrumb component and a form component.
    
    The commit also adds a new file `actions.ts` under the `app/lib` directory. This file contains code to handle creating an invoice, including validating form data and inserting the invoice into the database.
    
    Additionally, the commit modifies the existing file `create-form.tsx` under the `app/ui/invoices` directory. The modification includes importing the `createInvoice` function from `actions.ts`.
    
    Lastly, there is a minor modification in the existing file `search.tsx`. A console log statement has been removed from the handleSearch function.
    
    These changes add functionality for creating invoices and improve code organization.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,7 +10,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log('🚀 ~ handleSearch ~ term:', term);
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
     if (term) {
