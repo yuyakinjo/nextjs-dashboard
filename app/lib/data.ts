bece4276d23dd02c66d49cc2c9d42bec060e983c
yuya <kinjo.bobchin@gmail.com>
2023-10-30 12:24:11 +0900

    chapter9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,14 +35,20 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  console.log('Fetching Invoice data...');
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
   noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+    SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    ORDER BY invoices.date DESC
+    LIMIT 5`;
+
+    console.log('Data fetch complete after 5 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
